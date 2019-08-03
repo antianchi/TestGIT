6c499ba1d20fe71aecc73b54bb691da82c2308af
atc00@foxmail.com <atc00@foxmail.com>
2019-08-03 15:05:15 +0800

    My Fisrt in eclipse

---
+++ src/main/java/com/an/TestGIT/App.java
@@ -0,0 +1,13 @@
+package com.an.TestGIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
